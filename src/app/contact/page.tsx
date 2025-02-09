9edf7ba3ff514eb2f5af003ecfd43e11de0a9466
ahuva12 <ahuvaeshit@gmail.com>
2025-02-09 19:01:49 +0200

    delete import sendEmail

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import styles from './contact.module.css';
 import { useForm, SubmitHandler } from "react-hook-form";
-import { sendEmail } from '../../services/sendEmail';
 import { Contact } from '@/components';
 
 const ContactPage = () => {
